efd144fec9bc233f41439f5d3e0a3b117c7fda34
duckyduck01 <madhvetipathak2006@gmail.com>
2026-08-20 12:46:21 +0530

    initial commit

---
+++ whiteboard/asgi.py
@@ -0,0 +1,25 @@
+"""
+ASGI config for whiteboard project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'whiteboard.settings')
+
+asgi_application = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from board.routing import websocket_urlpatterns
+
+application=ProtocolTypeRouter({
+    'http':asgi_application,
+    'websocket':AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+})
